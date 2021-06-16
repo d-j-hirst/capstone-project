408eff45ae3512180ae2d88560664eb17643c8ca
djhir3 <djhir3@gmail.com>
2021-06-17 03:26:02 +1000

    Load database and create tables for models

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, request, abort, jsonify
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
+  setup_db(app)
   CORS(app)
 
   return app
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
+from sqlalchemy import Column, String, Integer, DateTime
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+# Get the database path from an environment variable
+# Heroku automatically generates a DATABASE_URL beginning with
+#  "postgres://" (which can't be easily edited), while SQLAlchemy in
+#  its latest version will only work with "postgresql://". This
+#  adjustment allows compatibility between the two by converting
+#  the scheme of Heroku's URL
+database_path = os.environ.get('DATABASE_URL').replace("s://", "sql://", 1)
+
+db = SQLAlchemy()
+
+# setup_db(app)
+# binds a flask application and a SQLAlchemy service
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    print('Databases created')
+    print(database_path)
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    release_date = Column(DateTime())
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer)
+    gender = Column(String)
